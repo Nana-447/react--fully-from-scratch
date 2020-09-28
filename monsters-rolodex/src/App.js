45ebfa0df5acee537f9bd43e9f8dd03bfdd20a03
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-09-28 00:34:54 -0300

    [CLASS] - OnClick Simple event

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,22 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super(); // CALL THE CONSTRUCTOR METHOD ON THE COMPONENT AND GIVES US ACCESS TO THE STATE
+
+    this.state = {
+      string: 'Hello Joao'
+    };
+  }
+  
   render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{ this.state.string }</p>
+          <button onClick = {() => this.setState({ string: "Hello Testing" })}>Change Text</button>
         </header>
       </div>
     );    
