2cae2b733ad0a8459c8920cacd91e586ed5f3c04
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-10-01 16:51:09 -0300

    [Monster rolodex] - setState callback

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,7 +9,8 @@ class App extends Component { // OR I CAN USE React.Component
     super(); // CALL THE CONSTRUCTOR METHOD ON THE COMPONENT AND GIVES US ACCESS TO THE STATE
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,7 +22,14 @@ class App extends Component { // OR I CAN USE React.Component
   
   render () {
     return (
-      <div className="App">
+      <div className='App'>
+        <input 
+          type='search' 
+          placeholder='Search monsters' 
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () => console.log(this.state)); 
+            }}
+        />
         <CardList monsters={this.state.monsters}/>
       </div>
     );    
