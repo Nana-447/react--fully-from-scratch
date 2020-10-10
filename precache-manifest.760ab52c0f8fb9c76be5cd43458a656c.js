eec04111b68d987b417c2d9b3b2c47ac6ae05714
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-10-10 17:08:00 -0300

    Updates

---
+++ precache-manifest.760ab52c0f8fb9c76be5cd43458a656c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a62b89f8acfd474addaefb9903bbc9",
+    "url": "/React--Zero-To-Mastery/index.html"
+  },
+  {
+    "revision": "4ca1faaa64211c216082",
+    "url": "/React--Zero-To-Mastery/static/css/main.89f0c2a2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React--Zero-To-Mastery/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React--Zero-To-Mastery/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca1faaa64211c216082",
+    "url": "/React--Zero-To-Mastery/static/js/main.052dbb4f.chunk.js"
+  },
+  {
+    "revision": "0e8eda578d18241aa2b9",
+    "url": "/React--Zero-To-Mastery/static/js/runtime-main.abfceb4c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React--Zero-To-Mastery/precache-manifest.760ab52c0f8fb9c76be5cd43458a656c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React--Zero-To-Mastery/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
