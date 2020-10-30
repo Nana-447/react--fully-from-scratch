66240e542d776936b8254218864e698f968f8355
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-10-29 23:17:30 -0300

    [CRWN Clothing] - Header Component with Link Router

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch> {/* Switch matches only one and nothing more after it. Unique router */}
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
