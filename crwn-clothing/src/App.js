ba5fd10e9b4a5d0cbbd66f7ed00729135680efa2
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-01 03:42:00 -0300

    [CRWN Clothing] - Using Firebase Google Authenticator (Calling Users and Usign Component Did Mount with Auth) 2

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -7,18 +7,46 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch> {/* Switch matches only one and nothing more after it. Unique router */}
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+// App.js converted to a Class Component
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  // Important to close Auth
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    // Method from Firebase that tells the App when the user has some change
+    // It works as a subscriber method that keeps listening to its changes    
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  // Important to close Auth
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch> {/* Switch matches only one and nothing more after it. Unique router */}
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
