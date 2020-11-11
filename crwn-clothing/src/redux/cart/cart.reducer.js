59d9b7c78b0ff89810d559fa6b39a3f3653810f7
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-11 19:34:23 -0300

    [CRWN Clothing] - Cart Item Reducer (Add Item)

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import CartActionTypes from './cart.types.js';
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden // inverse
+            }
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems: [...state.cartItems, action.payload] // Spreading the old values in the first attribute // Adding the second value to the array
             }
         default:
             return state;
