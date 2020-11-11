c087d5df2ad00b23a796dc53abd3f87a601bc6e1
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-10 23:17:04 -0300

    [CRWN Clothing] - CartDropdown component

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
-import CardIcon from '../cart-icon/cart-icon.component';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import CartIcon from '../cart-icon/cart-icon.component';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -37,6 +37,7 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
