ca884a68f9c9adade103d1411289c0f3f8469f33
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-12 20:24:42 -0300

    [CRWN Clothing] - CartItem Component // Using Connect to render by state

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => ( // Reading CartItems from connected reducer (Cart)
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            { cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} /> // Rendering CartItems based on the state read from redux connect
+            ))}
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+// mapStateToPros = reads the state value
+const mapStateToPros = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToPros)(CartDropdown);
