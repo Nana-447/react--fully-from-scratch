961a93005835f80c8f0c0affac8804b39f3c6283
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-29 22:52:50 -0300

    [CRWN Clothing] - Removing Redux Logger from Production

--- crwn-clothing/src/redux/store.js
+++ crwn-clothing/src/redux/store.js
@@ -4,7 +4,11 @@ import { persistStore } from 'redux-persist'; // It allows our browser to cache 
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development'){ // INSIDE OF NODE, THERE IS A ENVIROMENT VARIABLE. It's already configured when we create a react app
+    middlewares.push(logger);
+}
 
 // Spread in all of the methods or all of the values in this array [logger] into the function as arguments
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
