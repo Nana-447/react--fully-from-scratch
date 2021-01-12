e6db74782065cf72d50bd83d8faba346d6ac1216
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2021-01-11 22:37:30 -0300

    [CRWN Clothing] - Sign In (REDUX SAGA REFACTOR)

--- crwn-clothing/src/redux/user/user.sagas.js
+++ crwn-clothing/src/redux/user/user.sagas.js
@@ -2,7 +2,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { googleSignInSuccess, googleSignInFailure } from './user.actions';
+import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from './user.actions';
 
 import { 
     auth, 
@@ -23,10 +23,31 @@ export function* signInWithGoogle(){
     }
 }
 
+export function* signInWithEmail({payload: {email, password}}){
+    try {
+        const { user } = yield auth.signInWithEmailAndPassword(email, password);
+        const userRef = yield call(createUserProfileDocument, user);
+        const userSnapshot = yield userRef.get();
+        //Put: put things back to our Redux flow
+        yield put(emailSignInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+    } catch(error){
+        yield put(emailSignInFailure(error))
+    }
+}
+
 export function* onGoogleSignInStart(){
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* onEmailSignInStart(){
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
+}
+
 export function* userSagas(){
-    yield all([call(onGoogleSignInStart)]);
+    yield all(
+        [
+            call(onGoogleSignInStart),
+            call(onEmailSignInStart)
+        ]
+    );
 }
