71b94e60b070d009ddf5468a9f0afd3458e9585d
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2021-01-20 22:56:05 -0300

    [CRWN Clothing] - Backend Payment Route

--- crwn-clothing/server.js
+++ crwn-clothing/server.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// If it's different than production, the dotenv will search for .env file and load the parameters
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,4 +27,22 @@ if(process.env.NODE_ENV === 'production'){
 app.listen(port, error => {
     if (error) throw error;
     console.log('Server running on port ' + port);
+});
+
+// Route
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+    // Using stripe library
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr){
+            res.status(500).send({error: stripeErr});
+        }
+        else{
+            res.status(200).send({success: stripeRes});
+        }
+    });
 });
