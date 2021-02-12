c6b5f8085f51e0e32b9aa4df1a97f91c5136f42d
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2021-02-12 00:39:21 -0300

    [GraphQL with Apollo] - Adding Items to Apollo

--- crwn-clothing--using--graphql/src/graphql/resolvers.js
+++ crwn-clothing--using--graphql/src/graphql/resolvers.js
@@ -1,17 +1,30 @@
 import { gql } from 'apollo-boost';
 
+import { addItemToCart } from './cart.utils';
+
 // Type Definitions need to be upperCase
 export const typeDefs = gql`
+    extend type Item {
+        quantity: Int
+    }
+
     extend type Mutation {
         ToggleCartHidden: Boolean!
+        AddItemToCart(item: Item!): [Item]!
     }
-`
+`;
 
 const GET_CART_HIDDEN = gql`
     {
         cartHidden @client
     }
-`
+`;
+
+const GET_CART_ITEMS = gql`
+    {
+        cartItems @client
+    }
+`;
 
 export const resolvers = {
     Mutation: {
@@ -26,6 +39,21 @@ export const resolvers = {
             });
 
             return !cartHidden;
+        },
+
+        addItemToCart: (_root, { item }, { cache }) => {
+            const { cartItems } = cache.readQuery({
+                query: GET_CART_ITEMS
+            });
+
+            const newCartItems = addItemToCart(cartItems, item);
+
+            cache.writeQuery({
+                query: GET_CART_ITEMS,
+                data: { cartItems: newCartItems}
+            });
+
+            return newCartItems;
         }
     }
 }
